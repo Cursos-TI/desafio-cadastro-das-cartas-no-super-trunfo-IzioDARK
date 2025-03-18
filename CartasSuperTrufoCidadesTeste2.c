85d0b5eb89407eb354af0b47117474cae6deeae8
IzioDARK <sandroaluizio7@gmail.com>
2025-03-18 00:35:36 -0300

    Add files via upload

---
+++ CartasSuperTrufoCidadesTeste2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(){
+    char Carta = 01;
+    char Estado[50] = "Alagoas";
+    char Código[20] = "A01";
+    char Cidade[20] = "Maceio";
+    int População = 3127511;
+    float Área= 27848.140;
+    float PIB = 76.266;
+    int Numero = 27;
+
+    
+    printf("Carta %d\n", Carta);
+    printf("Estado: %s\n", Estado);
+    printf("Código: %s\n", Código);
+    printf("Cidade: %s\n", Cidade);
+    printf("População: %d\n", População);
+    printf("Área em Km²: %.3f\n", Área);
+    printf("PIB: %.3f\n", PIB);
+    printf("Numero de Pontos Turisticos: %d\n", Numero);
+    printf("\n");
+    printf("\n");
+    
+ 
+
+    char carta = 02;
+    char estado[50] = "Pernanbuco";
+    char código[20] = "A02";
+    char cidade[20] = "Recife";
+    int população = 9058155;
+    float área= 98067.881;
+    float pib = 245.828;
+    int numero = 32;
+
+    
+    printf("Carta %d\n", carta);
+    printf("Estado: %s\n", estado);
+    printf("Código: %s\n", código);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %d\n", população);
+    printf("Área em Km²: %.3f\n", área);
+    printf("PIB: %.3f\n", pib);
+    printf("Numero de Pontos Turisticos: %d\n", numero);
+}
