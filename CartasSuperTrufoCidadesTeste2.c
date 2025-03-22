00a97dd4d16702f4889356ddda5d6f943a63f72b
IzioDARK <sandroaluizio7@gmail.com>
2025-03-22 12:30:26 -0300

    Add files via upload

--- CartasSuperTrufoCidadesTeste2.c
+++ CartasSuperTrufoCidadesTeste2.c
@@ -9,20 +9,25 @@ int main(){
     float Área= 27848.140;
     float PIB = 76.266;
     int Numero = 27;
+    float Densidade;
+    float PIBper;
 
-    
+    Densidade = População / Área;
+    PIBper = População / PIB;
+
     printf("Carta %d\n", Carta);
     printf("Estado: %s\n", Estado);
     printf("Código: %s\n", Código);
     printf("Cidade: %s\n", Cidade);
-    printf("População: %d\n", População);
-    printf("Área em Km²: %.3f\n", Área);
+    printf("População: %d Habitantes\n", População);
+    printf("Área em Km²: %.3fKM²\n", Área);
     printf("PIB: %.3f\n", PIB);
     printf("Numero de Pontos Turisticos: %d\n", Numero);
+    printf("Densidade Populacional: %.2f Hab/Km² \n", Densidade);
+    printf("PIB per Capita: %.2f Reais\n", PIBper);
     printf("\n");
     printf("\n");
     
- 
 
     char carta = 02;
     char estado[50] = "Pernanbuco";
@@ -32,14 +37,23 @@ int main(){
     float área= 98067.881;
     float pib = 245.828;
     int numero = 32;
+    float densidade;
+    float pibPER;
+
+    densidade = população / área;
+    pibPER = população / pib;
 
     
     printf("Carta %d\n", carta);
     printf("Estado: %s\n", estado);
     printf("Código: %s\n", código);
     printf("Cidade: %s\n", cidade);
-    printf("População: %d\n", população);
-    printf("Área em Km²: %.3f\n", área);
+    printf("População: %d Habitantes\n", população);
+    printf("Área em Km²: %.3fKm²\n", área);
     printf("PIB: %.3f\n", pib);
     printf("Numero de Pontos Turisticos: %d\n", numero);
+    printf("Densidade Populacional: %.2f Hab/Km² \n", densidade);
+    printf("PIB per Capita: %.2f Reais\n", pibPER);
+
+    return 0;
 }
